b1db9d1fc93a019cfdb1721e8eac66683661f66b
leo2 <leo2@Jotunheim>
2016-12-11 18:01:51 +0100

    Bug fixes

--- chat/src/chat/Srv/ChatMessage.java
+++ chat/src/chat/Srv/ChatMessage.java
@@ -1,31 +1,37 @@
 package chat.Srv;
 
-import java.io.Serializable;
-
-public class ChatMessage implements Serializable{
+import java.io.*;
+/*
+ * This class defines the different type of messages that will be exchanged between the
+ * Clients and the Server. 
+ * When talking from a Java Client to a Java Server a lot easier to pass Java objects, no 
+ * need to count bytes or to wait for a line feed at the end of the frame
+ */
+public class ChatMessage implements Serializable {
 
 	protected static final long serialVersionUID = 1112122200L;
-	
-	public static final int LOGIN = 0;
+
+	// The different types of message sent by the Client
+	// WHOISIN to receive the list of the users connected
+	// MESSAGE an ordinary message
+	// LOGOUT to disconnect from the Server
+	public static final int WHOISIN = 0;
 	public static final int MESSAGE = 1;
 	public static final int LOGOUT = 2;
+	private int type;
+	private String message;
 	
-	private int _type;
-	private String _msg;
-	
-	public ChatMessage (int type, String msg)
-	{
-		_type = type;
-		_msg = msg;
+	// constructor
+	public ChatMessage(int type, String message) {
+		this.type = type;
+		this.message = message;
 	}
 	
-	public String getMessage() {
-		return _msg;
-	}
-
-
+	// getters
 	public int getType() {
-		return _type;
+		return type;
 	}
-
+	public String getMessage() {
+		return message;
+	}
 }
